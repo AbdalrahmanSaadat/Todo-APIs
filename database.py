aeaaa669887d2ecb30448d8fa10198f1bd7a8f13
Abdalrahman Saadat <139587515+AbdalrahmanSaadat@users.noreply.github.com>
2025-01-10 19:27:03 +0200

    adding all req for deplyment and deploy the project on google services

--- database.py
+++ database.py
@@ -1,11 +1,33 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from google.cloud import secretmanager
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+###################### Local Tesing
 
-MONGODB_URI = os.getenv("MONGODB_URI")
-DB_NAME = os.getenv("DB_NAME")
+# load_dotenv()
+
+# MONGODB_URI = os.getenv("MONGODB_URI")
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+##################### Production
+
+
+def get_secret(secret_id):
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/todo-447413/secrets/{secret_id}/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    return response.payload.data.decode("UTF-8")
+
+MONGODB_URI = get_secret("MONGODB_URI")
+
+#^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+DB_NAME = "todo_db"
 
 client = AsyncIOMotorClient(MONGODB_URI)
 db = client[DB_NAME]
